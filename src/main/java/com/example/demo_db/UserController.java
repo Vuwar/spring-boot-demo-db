24c45b82500c77bfc6929aa24c863d73621dc838
Vuwar <vuqarkrm1@gmail.com>
2025-04-29 21:33:59 +0400

    7th commit

--- src/main/java/com/example/demo_db/UserController.java
+++ src/main/java/com/example/demo_db/UserController.java
@@ -3,17 +3,42 @@ package com.example.demo_db;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller
+import java.util.List;
+
+@Controller("/users")
 public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @PostMapping
     public ResponseEntity<UserEntity> addUser(@RequestParam("name") String name, @RequestParam("password") String password) {
         UserEntity user = new UserEntity(name, password);
-        return new ResponseEntity<>(user, HttpStatus.CREATED);
+        UserEntity savedUser = userRepository.save(user);
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
     }
 
+    @GetMapping("/get-all")
+    public ResponseEntity<List<UserEntity>> getAllUsers() {
+        List<UserEntity> users = userRepository.findAll();
+        if (users.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return ResponseEntity.ok(users);
+    }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<UserEntity> getUserById(@PathVariable Long id) {
+        return userRepository.findById(id)
+                .map(user -> new ResponseEntity<>(user, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
 }
